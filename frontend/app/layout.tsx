45b39dbfab436a729cf394bb0f95ee372a95cece
Riya Sinha <riyasinha5319@gmail.com>
2026-02-22 04:55:22 +0530

    Updates

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,7 +1,4 @@
-"use client";
-
-import { GoogleOAuthProvider } from "@react-oauth/google";
-
+import Providers from "./providers";
 export const metadata = {
   title: "Voyage",
 };
@@ -10,9 +7,7 @@ export default function RootLayout({ children }: any) {
   return (
     <html lang="en">
       <body>
-        <GoogleOAuthProvider clientId="894705257860-cueoes9jk5doldd7vkbne9bkvkgjnn8t.apps.googleusercontent.com">
-          {children}
-        </GoogleOAuthProvider>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
