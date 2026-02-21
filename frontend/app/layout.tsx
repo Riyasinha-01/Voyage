1d7da71c400d295a61b127da6644d156662f4648
Riya Sinha <riyasinha5319@gmail.com>
2026-02-22 05:05:57 +0530

    Updates

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,22 @@
 import Providers from "./providers";
 export const metadata = {
   title: "Voyage",
+  description: "AI Powered Travel Planner",
+  openGraph: {
+    title: "Voyage",
+    description: "AI Powered Travel Planner",
+    url: "https://planwithvoyage.vercel.app",
+    siteName: "Voyage",
+    images: [
+      {
+        url: "/thumbnail.png", // image in public folder
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
 };
 
 export default function RootLayout({ children }: any) {
