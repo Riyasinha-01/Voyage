bcb29b543efe62b69a7d7edb02e7ffab57099437
Riya Sinha <riyasinha5319@gmail.com>
2026-02-22 05:19:59 +0530

    Updates

--- frontend/app/providers.tsx
+++ frontend/app/providers.tsx
@@ -4,7 +4,7 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 
 export default function Providers({ children }: any) {
   return (
-    <GoogleOAuthProvider clientId="YOUR_CLIENT_ID">
+    <GoogleOAuthProvider clientId="894705257860-cueoes9jk5doldd7vkbne9bkvkgjnn8t.apps.googleusercontent.com">
       {children}
     </GoogleOAuthProvider>
   );
